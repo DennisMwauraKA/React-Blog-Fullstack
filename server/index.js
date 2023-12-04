36f2ace4e63240013bfee5e970b7240e57a8686f
unknown <dennismwaura074@gmail.com>
2023-12-04 17:40:25 +0300

    update

--- server/index.js
+++ server/index.js
@@ -2,6 +2,6 @@ const express =require("express");
 const app =express()
 
 app.use('/',(req,res)=>{
-    res.send("server is running")
+    res.send("server is going mad running")
 })
 app.listen(5000,console.log("Server has started"))
