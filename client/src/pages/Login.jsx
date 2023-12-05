dc53a2fc7fa0508c91836a8f075f6b0613fe800a
unknown <dennismwaura074@gmail.com>
2023-12-05 08:46:13 +0300

    updated login

---
+++ client/src/pages/Login.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+function Login() {
+  return (
+    <div>
+      <form>
+
+        <input type='text' placeholder='Enter your name'/>
+      </form>
+    </div>
+  )
+}
+
+export default Login
