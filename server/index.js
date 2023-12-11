7b4bf1745dd72cd4498d004c1851875a54fd8d67
unknown <dennismwaura074@gmail.com>
2023-12-11 12:29:50 +0300

    added the endpoints

--- server/index.js
+++ server/index.js
@@ -1,16 +1,16 @@
 const express = require("express");
-const cors = require('cors')
+const cors = require("cors");
 const app = express();
 const mongoose = require("mongoose");
 const bcrypt = require("bcrypt");
 const saltRounds = 10;
-const myPlaintextPassword = 's0/\/\P4$$w0rD';
-const someOtherPlaintextPassword = 'not_bacon';
+const myPlaintextPassword = "s0//P4$$w0rD";
+const someOtherPlaintextPassword = "not_bacon";
 const User = require("./models/userModel");
 const port = 3000;
 
 app.use(express.json());
-app.use(cors())
+app.use(cors());
 mongoose
   .connect(
     "mongodb+srv://dennismwaura074:Dennis@cluster0.bqjkwle.mongodb.net/?retryWrites=true&w=majority"
@@ -29,13 +29,11 @@ app.get("/", (req, res) => {
   res.send("HEllo Server is ready");
 });
 
-
 app.post("/register", async (req, res) => {
   try {
     const { email, password } = req.body;
-    const user = new User({ email, password:bcrypt.hash(myPlaintextPassword, saltRounds).then(function(hash) {
-     password,hash
-  }) });
+    const hashedPassword = await bcrypt.hash(password, saltRounds);
+    const user = new User({ email, password:hashedPassword });
     await user.save();
 
     res.status(200).json({ message: "user saved successfully" });
@@ -45,12 +43,10 @@ app.post("/register", async (req, res) => {
   }
 });
 
-app.post('/login',(req,res)=>{
-try {
-  
-} catch (error) {
-  console.log(error);
-  res.status(500).json({message: error.message});
-  
-}
-})
+app.post("/login", (req, res) => {
+  try {
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ message: error.message });
+  }
+});
