6dc1d6a070fe65c12aa081d232e42f7c79e62a78
unknown <dennismwaura074@gmail.com>
2023-12-11 09:42:54 +0300

    cors

--- server/index.js
+++ server/index.js
@@ -1,10 +1,12 @@
 const express = require("express");
+const cors = require('cors')
 const app = express();
 const mongoose = require("mongoose");
 const User = require("./models/userModel");
 const port = 3000;
 
 app.use(express.json());
+app.use(cors())
 mongoose
   .connect(
     "mongodb+srv://dennismwaura074:Dennis@cluster0.bqjkwle.mongodb.net/?retryWrites=true&w=majority"
