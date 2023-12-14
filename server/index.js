cedce9e8aedb856b63682545c7410ff30958febe
unknown <dennismwaura074@gmail.com>
2023-12-15 00:52:31 +0300

    publish profile

--- server/index.js
+++ server/index.js
@@ -83,12 +83,11 @@ app.post("/login", async (req, res) => {
   }
 });
 
-const { token } = req.cookies;
-
-jwt.verify(token, secret, {}, (err, info) => {
-  if (err) {
-    console.log(err);
-    return res.status(401).json({ message: "Unauthorized" });
-  }
-  res.json(info);
+app.get("/profile", (req, res) => {
+  const {token} =req.cookies;
+  jwt.verify(token,secret,{},(err,info)=>{
+if (err) throw err;
+res.json(info)
+  })
+  res.json(req.cookies);
 });
