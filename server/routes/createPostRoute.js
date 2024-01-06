f44f1db5dbccaf556cf61df0d184c445ade2aa24
unknown <dennismwaura074@gmail.com>
2024-01-06 06:38:15 +0300

    minor-changes

--- server/routes/createPostRoute.js
+++ server/routes/createPostRoute.js
@@ -1,14 +1,5 @@
 const express = require("express");
-const cors = require("cors");
-router.use(
-  cors({
-    origin: [
-      "https://react-blog-client-omega.vercel.app",
-      "http://localhost:5173",
-    ],
-    methods: "POST",
-  })
-);
+
 const createPostController = require("../controllers/createPostController.js");
 
 const router = express.Router();
