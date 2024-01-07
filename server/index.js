6d83fcc48e85f4cea38c24c2c5c2ef30d4d8abb6
unknown <dennismwaura074@gmail.com>
2024-01-07 19:53:40 +0300

    updates to server

--- server/index.js
+++ server/index.js
@@ -6,15 +6,14 @@ const mongoose = require("mongoose");
 const path = require("path");
 const app = express();
 app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
 app.use(cookieParser());
 
 app.use(
   cors({
     credentials: true,
-    origin: [
-      "https://react-blog-client-omega.vercel.app",
-      "http://localhost:5173",
-    ],
+   
     allowedHeaders: ["Authorization", "Content-Type"],
     methods: ["GET", "HEAD", "PUT", "PATCH", "POST", "DELETE"],
   })
